43e26f2ea81937c77ec748e4577cbf8e4eacb4f4
Abdurrahman Aborazmeh <aborazmeh@yahoo.com>
2017-11-09 09:55:58 +0300

    Add Amiri package

---
+++ Amiri/tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿$ErrorActionPreference = 'Stop';
+
+function Get-CurrentDirectory
+{
+  $thisName = $MyInvocation.MyCommand.Name
+  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/alif-type/amiri/releases/download/0.109/amiri-0.109.zip'
+
+$fontHelpersPath = (Join-Path (Get-CurrentDirectory) 'FontHelpers.ps1')
+. $fontHelpersPath
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = Join-Path $Env:Temp $packageName
+  fileType      = 'zip'
+  url           = $url
+  checksum      = '97ee6e40d87f4b31de15d9a93bb30bf27bf308f0814f4ee9c47365b027402ad6'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$shell = New-Object -ComObject Shell.Application
+$fontsFolder = $shell.Namespace(0x14)
+
+$fontFiles = Get-ChildItem $packageArgs.unzipLocation -Recurse -Filter *.ttf
+
+$commands = $fontFiles |
+% { Join-Path $fontsFolder.Self.Path $_.Name } |
+? { Test-Path $_ } |
+% { "Remove-SingleFont '$_' -Force;" }
+
+$fontFiles |
+% { $commands += "Add-SingleFont '$($_.FullName)';" }
+
+$toExecute = ". $fontHelpersPath;" + ($commands -join ';')
+Start-ChocolateyProcessAsAdmin $toExecute
+
+Remove-Item $packageArgs.unzipLocation -Recurse
