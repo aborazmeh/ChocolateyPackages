fda520b700fea4ce416757942dd97fab89204dca
Abdurrahman Aborazmeh <aborazmeh@yahoo.com>
2017-11-09 10:10:52 +0300

    Add uninstall script

---
+++ Amiri/tools/chocolateyuninstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+function Get-CurrentDirectory
+{
+  $thisName = $MyInvocation.MyCommand.Name
+  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
+$fontHelpersPath = (Join-Path (Get-CurrentDirectory) 'FontHelpers.ps1')
+. $fontHelpersPath
+
+$packageName   = $env:ChocolateyPackageName
+$shell = New-Object -ComObject Shell.Application
+$fontsFolder = $shell.Namespace(0x14)
+
+$fontFiles = Get-ChildItem $fontsFolder.Self.Path -Recurse -Filter arial*.ttf
+
+$commands = $fontFiles |
+% { "Remove-SingleFont '$_' -Force;" }
+
+$toExecute = ". $fontHelpersPath;" + ($commands -join ';')
+Start-ChocolateyProcessAsAdmin $toExecute
