3258d31cf2e20274c84a700a690a7f64943a5c7c
Abdurrahman Aborazmeh <aborazmeh@yahoo.com>
2018-07-22 09:26:30 +0300

    Upgrade MultiMarkdown package to v6.3.2

--- MultiMarkdown/tools/chocolateyinstall.ps1
+++ MultiMarkdown/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/fletcher/MultiMarkdown-6/releases/download/6.3.0/MultiMarkdown-Windows-32-6.3.0.exe'
-$url64      = 'https://github.com/fletcher/MultiMarkdown-6/releases/download/6.3.0/MultiMarkdown-Windows-6.3.0.exe'
+$url        = 'https://github.com/fletcher/MultiMarkdown-6/releases/download/6.3.2/MultiMarkdown-Windows-32-6.3.2.exe'
+$url64      = 'https://github.com/fletcher/MultiMarkdown-6/releases/download/6.3.2/MultiMarkdown-Windows-6.3.2.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,9 +10,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'multimarkdown*'
-  checksum      = 'E88E0FF9C92173D6D50412EA002AD9D250F299376C1FBEA5C081B9447E5F4CBD'
+  checksum      = '154589f99c235560a82af868ff4c0ebef1bcbc8ba4a17a2b96d1878987c8d085'
   checksumType  = 'sha256'
-  checksum64    = '2A1646AADDE01EDCBEF8A5A9E8813DEDD992BA4D956BF8F1AE9B2DFC97FB43F5'
+  checksum64    = '51e88926109d24df5fb9acf1ea854a330d4745effd7624ad3a77cc8f4a2b98ee'
   checksumType64= 'sha256'
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/S'
