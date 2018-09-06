3aeec218ddfb611e48513e94b68c2842aa75243e
Abdurrahman Aborazmeh <aborazmeh@yahoo.com>
2018-09-06 03:44:42 +0300

    Add Cerebro package

---
+++ Cerebro/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/KELiON/cerebro/releases/download/v0.3.2/cerebro-setup-0.3.2.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'cerebro*'
+  checksum      = '666c14cf17ff7aaabb6ff6781006b28db25102a55ed500c3ea1fcb7db16f1214'
+  checksumType  = 'sha256'
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
