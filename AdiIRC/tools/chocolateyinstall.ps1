c64e67b2e686cb1adebf3443af4b4f0124ffccc2
Abdurrahman Aborazmeh <aborazmeh@yahoo.com>
2019-04-09 02:09:00 +0300

    upgrade AdiIrc

--- AdiIRC/tools/chocolateyinstall.ps1
+++ AdiIRC/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.adiirc.com/AdiIRC.3.2.exe'
-$url64      = 'https://www.adiirc.com/AdiIRC64.3.2.exe'
+$url        = 'https://www.adiirc.com/AdiIRC.3.3.exe'
+$url64      = 'https://www.adiirc.com/AdiIRC64.3.3.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,9 +10,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'AdiIRC*'
-  checksum      = '715EEE29A12DD59B33B55D3AE8C62738BB2270A7433E1AB0F9D5A61BA9B10471'
+  checksum      = 'C486E52BF1EC1EF9BC668675A6EFCD39143C9235B2890A5DDF782B4F0E7C3189'
   checksumType  = 'sha256'
-  checksum64    = 'B92344F09B1FD8706CCD5392877841DF95CD861138C40749B70E89323EACC999'
+  checksum64    = 'D974BD427BE660D3C54E88BAA898B7B64C58D0EF7A6C8043C32985BEC0026AB2'
   checksumType64= 'sha256'
 
   validExitCodes= @(1223, 0, 3010, 1641)
